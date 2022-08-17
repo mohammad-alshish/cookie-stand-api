c049542257f5b0dd67e65dec71876c72f979571e
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-08-17 23:27:12 +0300

    75% done

--- project/urls.py
+++ project/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/things/", include("things.urls")),
+    path("api/v1/cookie-stands/", include("cookie_stands.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path(
         "api/token/",
-        jwt_views.TokenObtainPairView.as_view(),
+        MyTokenObtainPairView.as_view(),
         name="token_obtain_pair",
     ),
     path(
